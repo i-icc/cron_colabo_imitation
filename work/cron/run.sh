d4e3fdcde24d5368cdbd4eb8f3bb7cb30273cc26
bo_neko <52018660+i-icc@users.noreply.github.com>
2025-02-05 13:59:00 +0900

    絶対パスを使用

--- work/cron/run.sh
+++ work/cron/run.sh
@@ -4,4 +4,4 @@ notebook_file=$1
 output_file="${notebook_file%.*}_output.ipynb"
 
 cd /home/jovyan/work
-jupyter nbconvert --execute "$notebook_file" --to notebook --output "$output_file"
+/usr/local/bin/jupyter nbconvert --execute "$notebook_file" --to notebook --output "$output_file"
